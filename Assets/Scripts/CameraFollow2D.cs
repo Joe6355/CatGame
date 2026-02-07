0e2aaeae9eda69362b315be8a1ce00b23e7e3d5c
Naofumich <naofumich@mail.ru>
2026-02-07 22:10:58 +0500

    Сделал: - добавил новый TileMap - работал с камерой и слоями пост обработки - раскидывал объекты в сцене

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public Transform target;      // герой
+    public float smoothSpeed = 5f;
+    public Vector2 offset;        // смещение по X и Y
+
+    private float fixedZ;
+
+    void Start()
+    {
+        // запоминаем Z камеры и больше его не меняем
+        fixedZ = transform.position.z;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = new Vector3(
+            target.position.x + offset.x,
+            target.position.y + offset.y,
+            fixedZ
+        );
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            desiredPosition,
+            smoothSpeed * Time.deltaTime
+        );
+    }
+}
