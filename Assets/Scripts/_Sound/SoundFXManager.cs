2f6b31e0c4ebf42a20ee54c8a61a3c4404d757e4
Joe6355 <Dudin1918@mail.ru>
2026-02-16 21:36:52 +0300

    Сделал:       - добавил SoundFXManager для проигрывания звуковых эффектов (одиночный клип / случайный из массива) с авто-удалением AudioSource после проигрывания       - добавил SoundMixerManager для управления громкостью Master / SFX / Music через AudioMixer (логарифмическая шкала dB)       - сделал настройку звуков в меню       - подключил слайдера

---
+++ Assets/Scripts/_Sound/SoundFXManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawntransform, float volume)
+    {
+        //spawn gammeObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawntransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+
+        audioSource.volume  = volume;
+
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayRandomSoundFXClip(AudioClip[] audioClip, Transform spawntransform, float volume)
+    {
+        //assign a random index
+        int rand = Random.Range(0, audioClip.Length);
+        //spawn gammeObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawntransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip[rand];
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
